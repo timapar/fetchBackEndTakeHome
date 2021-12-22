d26563339783d7f37032f8ba2ab42cbd5bdc4922
timapar <timothy_parrish@hotmail.com>
2021-12-22 14:44:39 -0500

    Add addPoints route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const { addPointsRouter } = require('./routes/addPoints')
 
 const app = express()
 const port = process.env.PORT || 1337
 
+app.use('/', express.json())
+
 app.get('/', (req, res) => {
   res.send('Fetch Backend Assessment API')
 })
+app.use('/points/add', addPointsRouter)
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
