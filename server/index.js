f2e3ecf329e5a896db63a0a5df74d62dd6e4aa9f
timapar <timothy_parrish@hotmail.com>
2021-12-22 14:53:15 -0500

    Add pointsBalance route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { addPointsRouter } = require('./routes/addPoints')
+const { pointsBalanceRouter } = require('./routes/pointsBalance')
 
 const app = express()
 const port = process.env.PORT || 1337
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
   res.send('Fetch Backend Assessment API')
 })
 app.use('/points/add', addPointsRouter)
+app.use('/points/balance', pointsBalanceRouter)
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
