07ddde89450f278eb70034d0f0e07c00b16e425d
timapar <timothy_parrish@hotmail.com>
2021-12-23 09:28:03 -0500

    Implement adding a transaction and sorting the log by timestamp

--- server/cache.js
+++ server/cache.js
@@ -1,4 +1,21 @@
 const transactionLog = []
 const pointsBalance = {'totalPoints' : 0}
 
-module.exports = { transactionLog, pointsBalance }
+const addTransaction = (entry) => {
+  transactionLog.push(entry)
+}
+
+const sortTransactions = () => {
+  transactionLog.sort((a, b) => {
+    const dateA = new Date(a.timestamp)
+    const dateB = new Date(b.timestamp)
+    return dateA - dateB
+  })
+}
+
+module.exports = { 
+  transactionLog, 
+  pointsBalance, 
+  addTransaction,
+  sortTransactions
+}
