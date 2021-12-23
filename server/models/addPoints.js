762d0aec2d70a01a901764696c8f62caacb375fd
timapar <timothy_parrish@hotmail.com>
2021-12-23 11:49:28 -0500

    Add comments to logic

--- server/models/addPoints.js
+++ server/models/addPoints.js
@@ -6,12 +6,20 @@ const {
 } = require('../cache')
 
 const addEntry = (logEntry) => {
+  // Get number of logs before adding the new entry
   const logCount = transactionLog.length
+  // Add entry to log in cache
   addTransaction(logEntry)
+  // Update the total available points in cache
   updatePointsBalance(logEntry.points)
+  
+  // If there were any entries in the log previously
   if (logCount > 0) {
+    // Create Date object for latest previous entry
     const latestEntryDate = new Date(transactionLog[logCount - 1].timestamp)
+    // Create Date object for new entry
     const newEntryDate = new Date(logEntry.timestamp)
+    // If new entry's timestamp is prior to latest previous entry, sort the whole log
     if (newEntryDate < latestEntryDate) sortTransactions()
   }
 }
